13496ed5601d7a68e51b90018fdda604ea009fb3
Tiago S Dutra <thisistiagosdutra@gmail.com>
2022-05-25 11:16:38 -0400

    added some more!

---
+++ 8-kyu/training-js-nine/answer.js
@@ -0,0 +1,12 @@
+function padIt(str,n){
+    let i = 0
+    while (i < n){
+      if(i%2==0){
+        str = "*" + str
+      }else {
+        str = str + '*'
+      } 
+     i++
+    }
+      return str
+    }
