1e1303dddc9f71e303197ebe0b9a09bb93271183
Tiago S Dutra <thisistiagosdutra@gmail.com>
2022-05-26 13:52:31 -0400

    added some more code wars!

---
+++ 8-kyu/training-js-12/answer.js
@@ -0,0 +1,12 @@
+function giveMeFive(obj){
+    //coding here 
+    let arr=[]
+    for(var key in obj){
+    if(key.length === 5)
+        arr.push(key)
+     if(obj[key].length === 5)
+        arr.push(obj[key])
+  
+    } 
+          return arr
+  }
