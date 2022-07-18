ee4fd9bd2330b6bae14f956a866c43b84ac5d608
Tiago S Dutra <thisistiagosdutra@gmail.com>
2022-07-18 14:04:20 -0400

    Added a new one, going with Fundamentals

---
+++ 8-kyu/even_or_odd.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+function even_or_odd(number) {
+  if (number % 2 === 0){
+    return 'Even'
+  }  else return 'Odd'
+}
